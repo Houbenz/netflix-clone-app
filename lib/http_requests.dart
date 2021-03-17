996ded63b07214d9051d0b682ccea525bb96a829
Hocine Benzagouta <hocine.benzagouta@gmail.com>
2021-03-17 18:08:23 +0100

    added requests

--- lib/http_requests.dart
+++ lib/http_requests.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
-import 'package:netflix_clone/beans/TopRated.dart';
 import 'beans/Movie.dart';
+import 'beans/MovieVideo.dart';
 
  const String API_KEY ="0ccbf2dab700f54911df768a72781699";
  const String DOMAIN = "api.themoviedb.org";
@@ -51,8 +51,41 @@ Future<List<Movie>> topRatedMovies() async{
   }
 
 }
+//
+// http.get(Uri.https(DOMAIN, "3/movie/videos/${e.id}",{
+// "api_key":API_KEY,
+// "language=":"en-US",
+// },));
+
+
+Future<List<MovieVideo>> getMovieVideosLink(int id) async{
+
+   http.Response response = await http.get(Uri.https(DOMAIN, "3/movie/$id/videos",{
+    "api_key":API_KEY,
+    "language=":"en-US",
+    },));
+
+     if(response.statusCode == 200){
+
+
+        Map<String,dynamic> decodedJson =  jsonDecode(response.body);
+
+        String encodedJson = jsonEncode(decodedJson["results"]);
+
+        print(encodedJson);
+
+        var results = jsonDecode(encodedJson);
+
+        List<MovieVideo> list =  results.map<MovieVideo>((e) => MovieVideo.fromJson(e)).toList();
+
+        return list;
+     }
+     else
+       print(response.body);
+}
+
 
 void main(){
-  //fetchMovie().then((value) => print(value));
-  topRatedMovies().then((value) => print(value));
+
+  getMovieVideosLink(755).then((value) => print(value));
 }
