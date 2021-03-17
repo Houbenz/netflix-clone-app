d317ca27f5a7ce0330e1b88957879b3c8873f199
Hocine Benzagouta <hocine.benzagouta@gmail.com>
2021-03-17 13:52:49 +0100

    created main page

---
+++ lib/main/mainPage.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/main/HomePage.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+//0ccbf2dab700f54911df768a72781699
+
+class _MainPageState extends State<MainPage> {
+
+  int _currentindex=0;
+
+
+  static List<Widget> _pagesOptions=[
+    HomePage(),
+    HomePage(),
+    HomePage(),
+  ];
+
+  void _onTappedItem(int index){
+
+    setState(() {
+      _currentindex = index;
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: Colors.black,
+
+
+      appBar: AppBar(
+        leading: Container(width: 0,height: 0,),
+        titleSpacing: -10,
+        title: Container(
+            child: Image(image: AssetImage("assets/n_logo.png"),width: 15,)),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 8),
+            child: IconButton(
+                icon: Icon(Icons.cast), onPressed: ()=> print("cast")),
+          ),
+          Container(
+              margin: EdgeInsets.only(right: 8),
+              child: IconButton(icon: Icon(Icons.search), onPressed: ()=> print("search"))),
+          Container(
+            margin: EdgeInsets.only(right: 16),
+              child: InkWell(child: Image(image: AssetImage("assets/blue.PNG"),width: 24,),))
+        ],
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentindex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home,),label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.video_collection_outlined),label: "Coming soon"),
+          BottomNavigationBarItem(icon: Icon(Icons.cloud_download_outlined),label: "Downloads"),
+        ],
+        onTap: _onTappedItem ,
+      ),
+
+      body: _pagesOptions[_currentindex],
+    );
+  }
+}
